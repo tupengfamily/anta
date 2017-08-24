55280e9659d21007ed40bc777be6035fa0e8b276
tupengfamily <tupengfamily@outlook.com>
2017-08-24 22:05:29 +0800

    add content

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 $(function(){
 	$(".navlist").mouseover(function(){
-		$(this).find(".nav1").show();
+		$(this).find(".nav1").show().end().siblings().find(".nav1").hide();
 		$(".nav2").show();
 	})
-	$(".navlist").mouseleave(function(){
-		$(this).find(".nav1").hide();
+	$(".nav").mouseleave(function(e){
 		$(".nav2").hide();
+		$(this).find(".nav1").hide();
 	})
+
+
 	$(".nav1:last").css({"left":"-300px"});
 	
 	$(".navlist>a").siblings().find("a").mouseover(function(){
@@ -16,4 +18,58 @@ $(function(){
 		$(this).css({"text-decoration":"none"});
 	})
 	
+	var oBanner=$(".banner");
+	var oPic=$(".pic");
+	var apicist=oPic.find("li");
+	var oList=$(".list");
+	var aList=oList.find("li");
+	var preWidth=apicist.eq(0).width();
+	apicist.eq(0).clone(true).appendTo(oPic);
+	
+	oPic.width((apicist.length+1)*preWidth);
+	var aWidth=document.documentElement.clientWidth;
+	$(".pic li").width(aWidth);
+	
+	var i=0;
+	var timer=setInterval(function(){
+				move();
+		},4000);
+			
+	function move(){
+		clearInterval(timer);
+		i++;
+		if(i==apicist.length+1){
+			i=1;
+			oPic.css("left",0);
+		}
+		if(i==-1){
+			i=apicist.length-1;
+			oPic.css("left",-apicist.length*preWidth)
+		}
+		oPic.stop().animate({"left":-i*preWidth},300);
+			 timer=setInterval(function(){
+				move();
+			 },4000);
+			 
+			 if(i==apicist.length){
+			   	aList.eq(0).addClass("banlist").siblings().removeClass("banlist");
+			   }else{
+			   	aList.eq(i).addClass("banlist").siblings().removeClass("banlist");
+			   }
+			 
+			 
+		}
+	
+				   
+		aList.hover(function(){
+			i=$(this).index()-1;
+				move();
+				clearInterval(timer);
+			},function(){move()})
+			   
+	$(window).resize(function(){
+		location.reload();
+	})
 })
+
+
