c91da3028e638fc75a6c82c93a8642e2d2451bfc
tupengfamily <tupengfamily@outlook.com>
2017-08-29 22:04:43 +0800

    add

--- js/xiangqing.js
+++ js/xiangqing.js
@@ -5,87 +5,229 @@ $(function(){
 		$(".navul .navul1").html(html);
 		//console.log(html);
 		//console.log(data);
-	})
-/*		$(".navlist").mouseenter(function(){
-		//$(this).find(".nav1").show().end().siblings().find(".nav1").hide();
-		$(".nav2").show();
-		//console.log($(this));
-		console.log(1);
-	})
-	$(".nav").mouseleave(function(e){
-		$(".nav2").hide();
-		$(this).find(".nav1").hide();
-		console.log(2);
+		//console.log($(".navlist"));
 	})
 
-
-	$(".nav1:last").css({"left":"-300px"});
-	
-	$(".navlist>a").siblings().find("a").mouseover(function(){
-		$(this).css({"text-decoration":"underline"});
-	})
-	$(".navlist>a").siblings().find("a").mouseleave(function(){
-		$(this).css({"text-decoration":"none"});
-	})*/
 	$(".navul1").mouseover(function(){
 		$(".nav2").show();
 	})
 	$(".navul1").mouseleave(function(){
 		$(".nav2").hide();
 	})
-	
-	var oBanner=$(".banner");
-	var oPic=$(".pic");
-	var apicist=oPic.find("li");
+
+
+	var aPrewidth=$(".pic li").eq(0).width();
+	var alength=$(".pic li").length;
 	var oList=$(".list");
 	var aList=oList.find("li");
-	var preWidth=apicist.eq(0).width();
-	apicist.eq(0).clone(true).appendTo(oPic);
-	
-	oPic.width((apicist.length+1)*preWidth);
+	//console.log(alength);
+	//console.log(aPrewidth);
 	var aWidth=document.documentElement.clientWidth;
+//	console.log(aWidth);
+	$(".pic").width(aPrewidth*alength);
 	$(".pic li").width(aWidth);
-	
-	var i=0;
 	var timer=setInterval(function(){
-				move();
-		},4000);
-			
+		move();
+	},4000)
+	var i=0;
 	function move(){
 		clearInterval(timer);
 		i++;
-		if(i==apicist.length+1){
-			i=1;
-			oPic.css("left",0);
+		if(i==alength){
+			i=0;
 		}
-		if(i==-1){
-			i=apicist.length-1;
-			oPic.css("left",-apicist.length*preWidth)
-		}
-		oPic.stop().animate({"left":-i*preWidth},300);
-			 timer=setInterval(function(){
-				move();
-			 },4000);
-			 
-			 if(i==apicist.length){
-			   	aList.eq(0).addClass("banlist").siblings().removeClass("banlist");
-			   }else{
-			   	aList.eq(i).addClass("banlist").siblings().removeClass("banlist");
-			   }
-			 
-			 
-		}
+		$(".pic").animate({"left":-i*aPrewidth},400);
+		timer=setInterval(function(){
+			move();
+		},4000)
+		
+		aList.eq(i).addClass("banlist").siblings().removeClass("banlist");		
+	}	
 	
-				   
+
+	
+	
 		aList.hover(function(){
 			i=$(this).index()-1;
 				move();
 				clearInterval(timer);
-			},function(){move()})
-			   
-	$(window).resize(function(){
-		location.reload();
+		},function(){move()})
+	
+	
+	//动态加载列表数据；
+	
+	$.get("../json/xiangqingdata.json",function(data){
+	/*var html="";
+	var arr=[];
+	for(var j in data){
+		console.log(j);
+		//console.log(data[j].minimg);
+		var dataimg=data[j].minimg;
+		arr.push(dataimg);
+		//console.log(dataimg);
+
+	html+="<div class='box'><div class='middleimg'><a href='detail.html?id=" + j + "' title='"+data[j].title+"'>"+
+	"<img src='"+data[j].imgsrc+"'/></a></div><ul class='minimg'><li><img src='"+data[j].minimg[0]+"'</li><li><img src='"+data[j].minimg[1]+"'</li></ul><p><span>￥</span>"+data[j].price+"</p>"+
+	"<h4><a href='"+data[j].title+"'>"+data[j].title+"</a></div>"
+		//console.log(html);
+	
+	}
+
+	
+	$(".imgbox").html(html);*/
+	
+	for(var item in data){
+		var html;
+		switch (data[item].minimg.length){
+			case 2:{
+				console.log(2);
+				html = `<div class="box">
+				<div class="middleimg">
+					<a href="detal.html?id=${item}" title="${data[item].title}">
+						<img src="${data[item].imgsrc}" />
+					</a>
+				</div>
+				<ul class="minimg">
+					<li>
+						<img src="${data[item].minimg[0]}" />
+					</li>
+					<li>
+						<img src="${data[item].minimg[1]}" />
+					</li>
+				</ul>
+				<p><span>￥</span>${data[item].price}</p>	
+				<h4>
+					<a href="detal.html?id=${item}" title="${data[item].title}">${data[item].title}</a>
+				</h4>
+			</div>`;
+			break;
+			}
+			case 3:{
+				console.log(3);
+				html = `<div class="box">
+				<div class="middleimg">
+					<a href="detal.html?id=${item}" title="${data[item].title}">
+						<img src="${data[item].imgsrc}" />
+					</a>
+				</div>
+				<ul class="minimg">
+					<li>
+						<img src="${data[item].minimg[0]}" />
+					</li>
+					<li>
+						<img src="${data[item].minimg[1]}" />
+					</li>
+					<li>
+						<img src="${data[item].minimg[2]}" />
+					</li>
+				</ul>
+				<p><span>￥</span>${data[item].price}</p>	
+				<h4>
+					<a href="detal.html?id=${item}" title="${data[item].title}">${data[item].title}</a>
+				</h4>
+			</div>`;
+			break;
+			}
+			case 4:{
+				console.log(4);
+				html = `<div class="box">
+				<div class="middleimg">
+					<a href="detal.html?id=${item}" title="${data[item].title}">
+						<img src="${data[item].imgsrc}" />
+					</a>
+				</div>
+				<ul class="minimg">
+					<li>
+						<img src="${data[item].minimg[0]}" />
+					</li>
+					<li>
+						<img src="${data[item].minimg[1]}" />
+					</li>
+					<li>
+						<img src="${data[item].minimg[2]}" />
+					</li>
+					<li>
+						<img src="${data[item].minimg[3]}" />
+					</li>
+				</ul>
+				<p><span>￥</span>${data[item].price}</p>	
+				<h4>
+					<a href="detal.html?id=${item}" title="${data[item].title}">${data[item].title}</a>
+				</h4>
+			</div>`;
+			break;
+			}
+			case 5:{
+				console.log(5);
+				html = `<div class="box">
+				<div class="middleimg">
+					<a href="detal.html?id=${item}" title="${data[item].title}">
+						<img src="${data[item].imgsrc}" />
+					</a>
+				</div>
+				<ul class="minimg">
+					<li>
+						<img src="${data[item].minimg[0]}" />
+					</li>
+					<li>
+						<img src="${data[item].minimg[1]}" />
+					</li>
+					<li>
+						<img src="${data[item].minimg[2]}" />
+					</li>
+					<li>
+						<img src="${data[item].minimg[3]}" />
+					</li>
+					<li>
+						<img src="${data[item].minimg[4]}" />
+					</li>
+				</ul>
+				<p><span>￥</span>${data[item].price}</p>	
+				<h4>
+					<a href="detal.html?id=${item}" title="${data[item].title}">${data[item].title}</a>
+				</h4>
+			</div>`;
+			break;
+			}
+		}
+		$(".imgbox").append($(html));
+	}
+		$(".minimg").hide();
+		$(".box").css({"border":0})
+		$(".box").mouseenter(function(){
+			
+			$(this).find(".minimg").stop().slideDown();
+			$(this).css({"border":"1px solid #ddd"});
+			var middle=$(this).find(".middleimg img");
+			var middleimg=$(this).find(".middleimg img").attr("src");
+			console.log(middleimg);
+			$(this).find(".minimg li").mouseenter(function(){
+/*				var minimg=Array.from($(this).find("img"));
+				console.log(minimg);
+				var */
+				
+				//console.log($(this).index());
+				var newmiddleimg = middleimg.slice(0, middleimg.lastIndexOf('.')-1) + ($(this).index()+1) + '.jpg';
+				console.log(newmiddleimg);
+				middle.attr({src:newmiddleimg});
+				
+			})
+		})
+		
+		$(".box").mouseleave(function(){
+			$(this).find(".minimg").stop().slideUp();
+			$(this).css({"border":0});
+			var newmiddleimg = middleimg.slice(0, middleimg.lastIndexOf('.')-1) +1+ '.jpg';
+			middle.attr({src:newmiddleimg});
+		})
+		
+		
 	})
+	
+	
+	
+	
+	
 	
 	
 	
